93b87258c067d451cce1638c3182ce03a1ec7302
Eric Rannestad <ericrannestad@gmail.com>
2022-06-28 17:08:35 -0600

    Bootstrap Inital Tests

--- .eleventy.js
+++ .eleventy.js
@@ -91,7 +91,7 @@ module.exports = function(eleventyConfig) {
 
   // Don't process folders with static assets e.g. images
   eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("static/img");
+  eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("admin");
   eleventyConfig.addPassthroughCopy("_includes/assets/css");
   eleventyConfig.addPassthroughCopy("_includes/assets/js");
